5ccebbf783616026f44a145b13e604932cf73517
meypanhawath <panhawath18@gmail.com>
2026-02-12 12:01:56 +0700

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+import { faHandPointRight } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-red-500 text-7xl">Homework</h1>
-    <ul className="list-disc">
-      <a href="./photo">
-          <li className="text-3xl font-bold my-10 animate-bounce ml-10 rainbow-color">Intercepting Route [ CLICK HERE ]</li>
-      </a>
-    </ul>
-
+      <h1 className="text-green-800 text-7xl font-bold ml-10 my-10">Assignment</h1>
+      <ul className="">
+        <a
+          href="./photo"
+          className="flex items-center gap-3 text-3xl font-bold my-10 ml-10 animate-bounce rainbow-color"
+        >
+          <FontAwesomeIcon icon={faHandPointRight} height={50} className="text-4xl" />
+          <span>Intercepting Route [ NO SCAM ]</span>
+        </a>
+      </ul>
     </>
   );
 }
